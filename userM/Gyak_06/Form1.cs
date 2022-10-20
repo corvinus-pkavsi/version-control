2edecfc9b4d854b70b330e4acf7be564ff0fff42
aron tarnoy <tarnoyaa@gmail.com>
2022-10-20 18:33:44 +0200

    getRates

--- userM/Gyak_06/Form1.cs
+++ userM/Gyak_06/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gyak_06.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,30 @@ namespace Gyak_06
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            getRates();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void getRates() 
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "Eur",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
