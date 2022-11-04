6ea87609a9bd57cb4e16f9fcb26c1e1f4374223a
aron tarnoy <tarnoyaa@gmail.com>
2022-11-04 19:49:47 +0100

    gyak6

---
+++ userM/gyak_6/Form1.cs
@@ -0,0 +1,35 @@
+﻿using gyak_6.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyak_6
+{
+    public partial class Form1 : Form
+    {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factroy;
+        public BallFactory Factory
+        {
+            get { return Factory; }
+            set { _factroy = value; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
